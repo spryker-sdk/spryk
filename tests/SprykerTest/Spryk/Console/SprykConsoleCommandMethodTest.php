60998c1d6539ed98f48e627cc7d9291d94c3f719
René Klatt <rene.klatt@spryker.com>
2018-05-14 13:42:23 +0000

    core-4817 fixed casing

--- tests/SprykerTest/Spryk/Console/SprykConsoleCommandMethodTest.php
+++ tests/SprykerTest/Spryk/Console/SprykConsoleCommandMethodTest.php
@@ -40,7 +40,7 @@ class SprykConsoleCommandMethodTest extends Unit
 
         $tester->execute($arguments);
 
-        $targetFile = $this->tester->getRootDirectory() . 'vendor/spryker/spryker/Bundles/Catface/src/Spryker/Zed/CatFace/Business/CatFaceFacade.php';
+        $targetFile = $this->tester->getRootDirectory() . 'vendor/spryker/spryker/Bundles/CatFace/src/Spryker/Zed/CatFace/Business/CatFaceFacade.php';
         $this->assertFileExists($targetFile);
         $fileContent = file_get_contents($targetFile);
 
@@ -63,7 +63,7 @@ class SprykConsoleCommandMethodTest extends Unit
         $tester->execute($arguments);
         $tester->execute($arguments);
 
-        $targetFile = $this->tester->getRootDirectory() . 'vendor/spryker/spryker/Bundles/Catface/src/Spryker/Zed/CatFace/Business/CatFaceFacade.php';
+        $targetFile = $this->tester->getRootDirectory() . 'vendor/spryker/spryker/Bundles/CatFace/src/Spryker/Zed/CatFace/Business/CatFaceFacade.php';
         $this->assertFileExists($targetFile);
         $fileContent = file_get_contents($targetFile);
 
