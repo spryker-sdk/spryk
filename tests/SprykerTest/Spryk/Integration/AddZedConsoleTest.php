8998b1dc07ad72990fcb7a3d35990524f7b03d1e
René Klatt <rene.klatt@spryker.com>
2018-05-16 14:44:43 +0000

    core-4817 added test

---
+++ tests/SprykerTest/Spryk/Integration/AddZedConsoleTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Spryk\Integration;
+
+use Codeception\Test\Unit;
+use Spryker\Spryk\Console\SprykRunConsole;
+
+/**
+ * Auto-generated group annotations
+ * @group SprykerTest
+ * @group Spryk
+ * @group Integration
+ * @group AddZedConsoleTest
+ * Add your own group annotations below this line
+ */
+class AddZedConsoleTest extends Unit
+{
+    protected const SPRYK_NAME = 'AddZedConsole';
+
+    /**
+     * @var \SprykerTest\SprykIntegrationTester
+     */
+    protected $tester;
+
+    /**
+     * @return void
+     */
+    public function testAddsZedConsole(): void
+    {
+        $command = new SprykRunConsole();
+        $tester = $this->tester->getConsoleTester($command, static::SPRYK_NAME);
+
+        $arguments = [
+            'command' => $command->getName(),
+            SprykRunConsole::ARGUMENT_SPRYK => static::SPRYK_NAME,
+            '--module' => 'FooBar',
+            '--consoleName' => 'FooBar',
+            '--consoleCommand' => 'spryker:spryker',
+        ];
+
+        $tester->execute($arguments, ['interactive' => false]);
+        $this->assertFileExists($this->tester->getModuleDirectory() . 'src/Spryker/Zed/FooBar/Communication/Plugin/Console/FooBarConsole.php');
+    }
+}
