4b4a465940c13958b771badba3ae486e022b2727
René Klatt <rene.klatt@spryker.com>
2018-05-16 14:36:22 +0000

    core-4817 added more spryks

---
+++ tests/SprykerTest/Spryk/Integration/AddZedDependencyProviderTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Spryk\Integration;
+
+use Codeception\Test\Unit;
+use Spryker\Spryk\Console\SprykRunConsole;
+
+/**
+ * Auto-generated group annotations
+ * @group SprykerTest
+ * @group Spryk
+ * @group Integration
+ * @group AddZedDependencyProviderTest
+ * Add your own group annotations below this line
+ */
+class AddZedDependencyProviderTest extends Unit
+{
+    protected const SPRYK_NAME = 'AddZedDependencyProvider';
+
+    /**
+     * @var \SprykerTest\SprykIntegrationTester
+     */
+    protected $tester;
+
+    /**
+     * @return void
+     */
+    public function testAddsZedDependencyProvider(): void
+    {
+        $command = new SprykRunConsole();
+        $tester = $this->tester->getConsoleTester($command, static::SPRYK_NAME);
+
+        $arguments = [
+            'command' => $command->getName(),
+            SprykRunConsole::ARGUMENT_SPRYK => static::SPRYK_NAME,
+            '--module' => 'FooBar',
+        ];
+
+        $tester->execute($arguments, ['interactive' => false]);
+        $this->assertFileExists($this->tester->getModuleDirectory() . 'src/Spryker/Zed/FooBar/FooBarDependencyProvider.php');
+    }
+}
