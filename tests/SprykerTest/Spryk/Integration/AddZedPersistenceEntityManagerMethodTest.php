0165584d3ab88fcaa63ac7b3d2929b2fadda82dd
René Klatt <rene.klatt@spryker.com>
2018-05-25 11:48:44 +0000

    core-4817 added AddZedPersistenceEntityManagerMethod Spryk

---
+++ tests/SprykerTest/Spryk/Integration/AddZedPersistenceEntityManagerMethodTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Spryk\Integration;
+
+use Codeception\Test\Unit;
+use Spryker\Zed\FooBar\Persistence\FooBarEntityManager;
+
+/**
+ * Auto-generated group annotations
+ * @group SprykerTest
+ * @group Spryk
+ * @group Integration
+ * @group AddZedPersistenceEntityManagerMethodTest
+ * Add your own group annotations below this line
+ */
+class AddZedPersistenceEntityManagerMethodTest extends Unit
+{
+    /**
+     * @var \SprykerTest\SprykIntegrationTester
+     */
+    protected $tester;
+
+    /**
+     * @return void
+     */
+    public function testAddsZedPersistenceRepositoryMethod(): void
+    {
+        $this->tester->run($this, [
+            '--module' => 'FooBar',
+            '--method' => 'doSomething',
+            '--input' => 'string $fooBar',
+            '--output' => 'array',
+        ]);
+
+        $this->tester->assertClassHasMethod(FooBarEntityManager::class, 'doSomething');
+    }
+}
