d59c8cf1098f8ad7ffc7c139bd959e2ae6bf3383
Filip Sushko <filip.sushko@spryker.com>
2018-12-18 15:36:33 +0200

    Added tests

--- tests/SprykTest/Spryk/Integration/AddSprykerToolingTest.php
+++ tests/SprykTest/Spryk/Integration/AddSprykerToolingTest.php
@@ -8,6 +8,7 @@
 namespace SprykTest\Spryk\Integration;
 
 use Codeception\Test\Unit;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Auto-generated group annotations
@@ -25,15 +26,62 @@ class AddSprykerToolingTest extends Unit
     protected $tester;
 
     /**
-     * @return void
+     * @return array
      */
-    public function testAddsToolingFile(): void
+    public function testAddsToolingFile(): array
     {
         $this->tester->run($this, [
             '--module' => 'FooBar',
             '--organization' => 'Spryker',
         ]);
 
-        static::assertFileExists($this->tester->getModuleDirectory() . 'tooling.yml');
+        static::assertFileExists($this->getToolingFilePath());
+
+        return $this->getToolingConfigByFilePath($this->getToolingFilePath());
+    }
+
+    /**
+     * @depends testAddsToolingFile
+     *
+     * @param array $toolingConfig
+     *
+     * @return void
+     */
+    public function testChecksToolingConfigParams(array $toolingConfig): void
+    {
+        static::assertEquals($this->getDefaultToolingConfig(), $toolingConfig);
+    }
+
+    /**
+     * @return array
+     */
+    protected function getDefaultToolingConfig(): array
+    {
+        return [
+            'architecture-sniffer' => [
+                'priority' => 2,
+            ],
+            'code-sniffer' => [
+                'level' => 1,
+            ],
+        ];
+    }
+
+    /**
+     * @param string $toolingFilePath
+     *
+     * @return array
+     */
+    protected function getToolingConfigByFilePath(string $toolingFilePath): array
+    {
+        return Yaml::parseFile($toolingFilePath);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getToolingFilePath(): string
+    {
+        return $this->tester->getModuleDirectory() . 'tooling.yml';
     }
 }
