d94e2ecff009d1926a4caf3ef75a31036fbc033a
Filip Sushko <filip.sushko@spryker.com>
2018-12-20 12:58:46 +0200

    Fixed tests

--- tests/SprykTest/Spryk/Integration/AddSprykerModuleTest.php
+++ tests/SprykTest/Spryk/Integration/AddSprykerModuleTest.php
@@ -14,7 +14,7 @@ use Codeception\Test\Unit;
  * @group SprykerTest
  * @group Spryk
  * @group Integration
- * @group CreateSprykerModuleTest
+ * @group AddSprykerModuleTest
  * Add your own group annotations below this line
  */
 class AddSprykerModuleTest extends Unit
