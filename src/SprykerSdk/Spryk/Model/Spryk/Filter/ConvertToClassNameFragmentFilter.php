c66d40032b381bd3f6c778c550b07cb709ea3dd4
René Klatt <rene.klatt@spryker.com>
2019-04-30 11:52:46 +0000

    TE-3429 Added filter

---
+++ src/SprykerSdk/Spryk/Model/Spryk/Filter/ConvertToClassNameFragmentFilter.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerSdk\Spryk\Model\Spryk\Filter;
+
+use Zend\Filter\FilterChain;
+use Zend\Filter\Word\UnderscoreToCamelCase;
+
+class ConvertToClassNameFragmentFilter implements FilterInterface
+{
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'convertToClassNameFragment';
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    public function filter(string $value): string
+    {
+        return str_replace(DIRECTORY_SEPARATOR, '\\', rtrim($value, '\\/'));
+    }
+}
