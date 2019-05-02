4d8c8703f91611bd967adaa770714aa41677d33e
René Klatt <rene.klatt@spryker.com>
2019-05-02 11:54:13 +0000

    TE-3428 Added missing ignored files

---
+++ tests/_support/Fixtures/vendor/spryker/spryker/Bundles/ZipZap/src/Spryker/Service/ZipZap/ZipZapServiceInterface.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Service\ZipZap;
+
+use Generated\Shared\Transfer\FooBarTransfer;
+use Generated\Shared\Transfer\ZipZapTransfer;
+
+interface ZipZapServiceInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
+     * @param string $foo
+     *
+     * @return bool
+     */
+    public function methodWithStringArgument(string $foo): bool;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
+     * @param array $foo
+     *
+     * @return bool
+     */
+    public function methodWithArrayArgument(array $foo): bool;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
+     * @param string $foo
+     *
+     * @return void
+     */
+    public function methodReturnsVoid(string $foo): void;
+
+    /**
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\ZipZapTransfer $zipZapTransfer
+     *
+     * @return \Generated\Shared\Transfer\FooBarTransfer
+     */
+    public function methodWithTransferInputAndTransferOutput(ZipZapTransfer $zipZapTransfer): FooBarTransfer;
+
+    /**
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\ZipZapTransfer|null $zipZapTransfer
+     *
+     * @return bool
+     */
+    public function methodWithDefaultNull(?ZipZapTransfer $zipZapTransfer = null): bool;
+
+    /**
+     * @api
+     *
+     * @param array $foo
+     *
+     * @return bool
+     */
+    public function methodWithDefaultArray($foo = []): bool;
+
+    /**
+     * @api
+     *
+     * @return void
+     */
+    public function methodWithoutMethodReturnType();
+
+    /**
+     * @api
+     *
+     * @return
+     */
+    public function methodWithoutDocBlockReturnType();
+
+    /**
+     * @api
+     *
+     * @return string|int
+     */
+    public function methodWithMultipleReturn();
+
+    /**
+     * @api
+     *
+     * @return string|int|null
+     */
+    public function methodWithMultipleReturnAndNullable();
+
+    /**
+     * @api
+     *
+     * @return string|null
+     */
+    public function methodWithNullableReturn();
+}
