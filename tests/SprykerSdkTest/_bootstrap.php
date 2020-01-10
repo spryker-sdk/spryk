caec9095bb7234ec2cb35ce6efe2c46e3228b264
René Klatt <rene.klatt@spryker.com>
2020-01-10 08:48:03 +0000

    WIP

--- tests/SprykerSdkTest/_bootstrap.php
+++ tests/SprykerSdkTest/_bootstrap.php
@@ -3,3 +3,9 @@
 if (!defined('APPLICATION_ROOT_DIR')) {
     define('APPLICATION_ROOT_DIR', codecept_data_dir());
 }
+if (!defined('APPLICATION_ENV')) {
+    define('APPLICATION_ENV', 'development');
+}
+if (!defined('APPLICATION_STORE')) {
+    define('APPLICATION_STORE', 'US');
+}
