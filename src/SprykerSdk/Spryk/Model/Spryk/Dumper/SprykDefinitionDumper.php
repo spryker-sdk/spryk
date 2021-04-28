78090b6680a5c2f657ccf7dad59753bd3382675d
oleksandr-stoilovskyi <oleksandr.stoilovskyi@spryker.com>
2021-04-28 09:07:28 +0300

    APPS-8 small fixes after CR

--- src/SprykerSdk/Spryk/Model/Spryk/Dumper/SprykDefinitionDumper.php
+++ src/SprykerSdk/Spryk/Model/Spryk/Dumper/SprykDefinitionDumper.php
@@ -44,7 +44,7 @@ class SprykDefinitionDumper implements SprykDefinitionDumperInterface
             $sprykName = str_replace('.' . $fileInfo->getExtension(), '', $fileInfo->getFilename());
             $sprykDefinition = $this->configurationLoader->loadSpryk($sprykName);
 
-            if ($level === (int) $sprykDefinition['level'] || $level === null) {
+            if ($level === null || $level === (int) $sprykDefinition['level']) {
                 $sprykDefinitions[$sprykName] = $sprykDefinition;
             }
         }
