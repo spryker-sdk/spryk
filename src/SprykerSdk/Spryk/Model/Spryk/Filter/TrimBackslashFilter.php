f80fee90aba70541d6fe9a6e4f0940414eb00542
oleksandr-stoilovskyi <oleksandr.stoilovskyi@spryker.com>
2021-05-25 19:07:54 +0300

    [APPS-612] small refactoring

--- src/SprykerSdk/Spryk/Model/Spryk/Filter/TrimBackslashFilter.php
+++ src/SprykerSdk/Spryk/Model/Spryk/Filter/TrimBackslashFilter.php
@@ -24,8 +24,6 @@ class TrimBackslashFilter implements FilterInterface
      */
     public function filter(string $value): string
     {
-        $namespace = trim($value, '\\');
-
-        return $namespace;
+        return trim($value, '\\');
     }
 }
