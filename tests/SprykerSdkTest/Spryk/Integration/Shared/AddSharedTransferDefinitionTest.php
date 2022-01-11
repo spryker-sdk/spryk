73a8ec983fc832c72cb84e6299b98dce6fda5d16
René Klatt <rene.klatt@spryker.com>
2022-01-11 11:29:36 +0100

    Removed invalid namespace from XML.

--- tests/SprykerSdkTest/Spryk/Integration/Shared/AddSharedTransferDefinitionTest.php
+++ tests/SprykerSdkTest/Spryk/Integration/Shared/AddSharedTransferDefinitionTest.php
@@ -39,7 +39,7 @@ class AddSharedTransferDefinitionTest extends Unit
         $this->assertXmlStringEqualsXmlFile(
             $this->tester->getModuleDirectory() . 'src/Spryker/Shared/FooBar/Transfer/foo_bar.transfer.xml',
             '<?xml version="1.0"?>
-<transfers xmlns="spryker:transfer-01" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="spryker:transfer-01 http://static.spryker.com/transfer-01.xsd">
+<transfers xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="spryker:transfer-01 http://static.spryker.com/transfer-01.xsd">
   <transfer name="FooBarItem"/>
 </transfers>',
         );
@@ -59,7 +59,7 @@ class AddSharedTransferDefinitionTest extends Unit
         $this->assertXmlStringEqualsXmlFile(
             $this->tester->getProjectModuleDirectory('FooBar', 'Shared') . 'Transfer/foo_bar.transfer.xml',
             '<?xml version="1.0"?>
-<transfers xmlns="spryker:transfer-01" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="spryker:transfer-01 http://static.spryker.com/transfer-01.xsd">
+<transfers xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="spryker:transfer-01 http://static.spryker.com/transfer-01.xsd">
   <transfer name="FooBarItem"/>
 </transfers>',
         );
