aefcef67ef16caa9eec1309e4731e9d4f55168b8
Burnashov Andrew <burnashov.a-dev@adict.ru>
2022-03-22 11:40:38 +0300

    APPS-3490 Create AddCrudFacade Spryk
    * Added a yaml file for the spryk

--- tests/SprykerSdkTest/Spryk/Integration/Spryker/Zed/Business/AddCrudFacadeTest.php
+++ tests/SprykerSdkTest/Spryk/Integration/Spryker/Zed/Business/AddCrudFacadeTest.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
 namespace SprykerSdkTest\Spryk\Integration\Spryker\Zed\Business;
 
 use Codeception\Test\Unit;
