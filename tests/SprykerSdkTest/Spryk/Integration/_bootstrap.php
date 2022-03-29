c99cf4e684a13f893d11ae40e353ab9b35ce856a
René Klatt <rene.klatt@spryker.com>
2022-03-29 12:40:29 +0200

    Feature/phar (#108)
    * Added spryk.phar.
    * Moved code to new repository.
    
    Co-authored-by: Anton Zubariev <anton.zubariev@spryker.com>
    Co-authored-by: prashant-malav-metacube <96427507+prashant-malav-metacube@users.noreply.github.com>
    Co-authored-by: vol4onok <alexander.veselov@spryker.com>

--- tests/SprykerSdkTest/Spryk/Integration/_bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!defined('APPLICATION_ROOT_DIR')) {
-    define('APPLICATION_ROOT_DIR', codecept_data_dir() . '/../../');
-}
-
-if (!defined('APPLICATION_ENV')) {
-    define('APPLICATION_ENV', 'devtest');
-}
